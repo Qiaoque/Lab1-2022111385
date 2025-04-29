d659cc2c496dbe9c2e350787067f049bb10e9a7c
Qiaoque <1109884939@qq.com>
2025-04-29 21:22:01 +0800

    C4 modified

--- include/Tools.h
+++ include/Tools.h
@@ -1,6 +1,6 @@
 #ifndef TOOLS_H
 #define TOOLS_H
-
+// C4 git 修改
 #include "Graph.h"
 std::string normalizeWord(const std::string& word);
 void displayShortestPath(const std::pair<double, std::vector<std::string>>& pathInfo);
